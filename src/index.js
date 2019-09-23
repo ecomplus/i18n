d899c94965a72b19a50a1388878c1e6d49a7d2c2
Leonardo Matos <leomp120894@gmail.com>
2019-09-23 19:59:12 -0300

    feat: setup index with basic strings

---
+++ src/index.js
@@ -0,0 +1,194 @@
+export const close = {
+  en_us: 'Close',
+  pt_br: 'Fechar'
+}
+
+export const remove = {
+  en_us: 'Remove',
+  pt_br: 'Remover'
+}
+
+export const shoppingCart = {
+  en_us: 'Shopping cart',
+  pt_br: 'Carrinho de compras'
+}
+
+export const myCart = {
+  en_us: 'My cart',
+  pt_br: 'Meu carrinho'
+}
+
+export const checkout = {
+  en_us: 'Checkout',
+  pt_br: 'Finalizar compra'
+}
+
+export const seeCart = {
+  en_us: 'See shopping cart',
+  pt_br: 'Ver carrinho'
+}
+
+export const emptyCart = {
+  en_us: 'Your shopping cart is empty',
+  pt_br: 'Seu carrinho de compras está vazio'
+}
+
+export const continueShopping = {
+  en_us: 'Continue shopping',
+  pt_br: 'Continuar comprando'
+}
+
+export const myAccount = {
+  en_us: 'My account',
+  pt_br: 'Minha conta'
+}
+
+export const myOrders = {
+  en_us: 'My orders',
+  pt_br: 'Meus pedidos'
+}
+
+export const hello = {
+  en_us: 'Hello',
+  pt_br: 'Olá'
+}
+
+export const visitor = {
+  en_us: 'visitor',
+  pt_br: 'visitante'
+}
+
+export const signInWith = {
+  en_us: 'Sign in with',
+  pt_br: 'Entrar com'
+}
+
+export const signUpWith = {
+  en_us: 'Sign up with',
+  pt_br: 'Cadastrar via'
+}
+
+export const another = {
+  en_us: 'another',
+  pt_br: 'outro'
+}
+
+export const email = {
+  en_us: 'email',
+  pt_br: 'e-mail'
+}
+
+export const logout = {
+  en_us: 'Logout',
+  pt_br: 'Sair'
+}
+
+export const login = {
+  en_us: 'Login',
+  pt_br: 'Entrar'
+}
+
+export const oauthOnPopup = {
+  en_us: 'Continue login on popup',
+  pt_br: 'Continue o login no pop-up'
+}
+
+export const loginError = {
+  en_us: 'There was an error trying to login, please try again or select another option',
+  pt_br: 'Houve um erro ao tentar o login, por favor tente novamente ou selecione outra opção'
+}
+
+export const profileNotFound = {
+  en_us: 'No profile found with email address',
+  pt_br: 'Nenhum perfil encontrado com o endereço de e-mail'
+}
+
+export const visitorCheckout = {
+  en_us: 'You can also buy as a visitor if you prefer',
+  pt_br: 'Você também pode comprar como visitante se preferir'
+}
+
+export const didYouMean = {
+  en_us: 'Did you mean',
+  pt_br: 'Você quis dizer'
+}
+
+export const noResultsFor = {
+  en_us: 'No results for',
+  pt_br: 'Nenhum produto encontrado para'
+}
+
+export const totalResultsFor = {
+  en_us: 'products found for',
+  pt_br: 'produtos encontrados para'
+}
+
+export const seeAll = {
+  en_us: 'See all',
+  pt_br: 'Ver todos'
+}
+
+export const from = {
+  en_us: 'from',
+  pt_br: 'de'
+}
+
+export const to = {
+  en_us: 'to',
+  pt_br: 'por'
+}
+
+export const unavailable = {
+  en_us: 'Unavailable',
+  pt_br: 'Indisponível'
+}
+
+export const outOfStock = {
+  en_us: 'Out of stock',
+  pt_br: 'Sem estoque'
+}
+
+export const buy = {
+  en_us: 'Buy',
+  pt_br: 'Comprar'
+}
+
+export const upTo = {
+  en_us: 'up to',
+  pt_br: 'até'
+}
+
+export const zip = {
+  en_us: 'Zip code',
+  pt_br: 'CEP'
+}
+
+export const calculateShipping = {
+  en_us: 'Calculate shipping',
+  pt_br: 'Calcular frete e prazo'
+}
+
+export const days = {
+  en_us: 'days',
+  pt_br: 'dias'
+}
+
+export const workingDays = {
+  en_us: 'working days',
+  pt_br: 'dias úteis'
+}
+
+export const freeShipping = {
+  en_us: 'free shipping',
+  pt_br: 'frete grátis'
+}
+
+export const interestFree = {
+  en_us: 'interest free',
+  pt_br: 'sem juros'
+}
+
+export const discountOf = {
+  en_us: 'discount of',
+  pt_br: 'desconto de'
+}
