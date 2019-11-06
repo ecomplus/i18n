c0806e660f1c03dbf7e4b9dec3f4c8cb3464cda6
Leonardo Matos <leomp120894@gmail.com>
2019-11-06 19:54:08 -0300

    feat(exports): multiple exports options on default entry file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+// named exports by lang
 import * as enUs from './en_us/'
 import * as ptBr from './pt_br/'
-
 export { enUs, ptBr }
 
+// named exports off all words in en-US (default language)
+export * from './en_us/'
+
+// default export dictionary object
+// all words and langs
 const dictionary = {}
 for (const str in enUs) {
   if (enUs[str]) {
@@ -14,5 +19,4 @@ for (const str in enUs) {
     }
   }
 }
-
 export default dictionary
